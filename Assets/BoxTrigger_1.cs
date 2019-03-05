8836985c47ad6eb8c8a88e33b791894bb6f46816
lin <zl1836@nyu.edu>
2019-03-04 20:57:03 -0500

    version 3.0

---
+++ Assets/BoxTrigger_1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTrigger_1 : MonoBehaviour
+{
+    private bool TargetStand = false;
+    public bool TargetStood = false;
+    public GameObject[] targets;
+    Vector3 tpos;
+    float control;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(TargetStand == true&&TargetStood==false)
+        {
+            foreach(GameObject target in targets)
+            {
+                print(target.transform.eulerAngles.x);
+                if (target.transform.eulerAngles.x <= 270 && target.transform.eulerAngles.x != 0)
+                {
+                    TargetStood = true;
+                  }
+                else
+                {
+                    target.transform.Rotate(0, 1, 0);
+                    //       tpos = target.transform.rotation.eulerAngles;
+                    //control = tpos.x;
+                    //tpos.x -= 1;
+
+                    //target.transform.eulerAngles = tpos;
+                }
+            }
+        }
+        if (TargetStood == true)
+        {
+            foreach(GameObject target in targets)
+            {
+                if(target.GetComponent<collsionTrigger>().hit == true)
+                {
+
+                    target.transform.Rotate(0, -1, 0);
+
+                }
+            }
+        }
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            TargetStand = true;
+        }
+    }
+}
---
+++ Assets/TimerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
